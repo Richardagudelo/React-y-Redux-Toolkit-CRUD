1e8f9105acedef36bf62c152b1003fc000ea93f6
RichardAgudelo <richard.agudelo@uptc.edu.co>
2023-05-18 00:29:30 -0500

    solucion error crud redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
 import TaskForm from "./components/TaskForm";
 import TasksList from "./components/TasksList";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="bg-zinc-900 h-screen text-white">
       <div className="flex items-center justify-center h-full">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<TasksList />} />
-          <Route path="/create-task" element={<TaskForm />} />
-          <Route path="/edit-task/:id" element={<TaskForm />} />
-        </Routes>
-      </BrowserRouter>
+        <Router basename="/React-y-Redux-Toolkit-CRUD">
+          <Routes>
+            <Route path="/" element={<TasksList />} />
+            <Route path="/create-task" element={<TaskForm />} />
+            <Route path="/edit-task/:id" element={<TaskForm />} />
+          </Routes>
+        </Router>
       </div>
     </div>
   )
 }
 
 export default App;
+
+// import TaskForm from "./components/TaskForm";
+// import TasksList from "./components/TasksList";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <div className="bg-zinc-900 h-screen text-white">
+//       <div className="flex items-center justify-center h-full">
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<TasksList />} />
+//           <Route path="/create-task" element={<TaskForm />} />
+//           <Route path="/edit-task/:id" element={<TaskForm />} />
+//         </Routes>
+//       </BrowserRouter>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App;
