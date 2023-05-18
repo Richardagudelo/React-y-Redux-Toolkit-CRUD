0d1c1c14e3035469b6fc54bb985c329bc8b9a7ce
RichardAgudelo <richard.agudelo@uptc.edu.co>
2023-05-18 01:02:43 -0500

    solucion error agregar base a vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import TaskForm from "./components/TaskForm";
 import TasksList from "./components/TasksList";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter , Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="bg-zinc-900 h-screen text-white">
       <div className="flex items-center justify-center h-full">
-        <Router basename="/React-y-Redux-Toolkit-CRUD">
+        <HashRouter >
+        {/* <HashRouter basename="/React-y-Redux-Toolkit-CRUD"> */}
           <Routes>
             <Route path="/" element={<TasksList />} />
             <Route path="/create-task" element={<TaskForm />} />
             <Route path="/edit-task/:id" element={<TaskForm />} />
           </Routes>
-        </Router>
+        </HashRouter>
       </div>
     </div>
   )
